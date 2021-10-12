13406969b98e3bc0d7474080318ae60dd4527837
Timothée <tnguyen@normandiewebschool.fr>
2021-10-12 11:54:43 +0200

    Création du controleur, de la vue et du modèle

---
+++ VueControlleur/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VueControlleur.Models;
+
+namespace VueControlleur.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("StudentView", new []
+            {
+                new StudentModel
+                {
+                    Prenom = "Raphaël",
+                    Nom = "Masse",
+                    Langage = "PHP"
+                },
+                new StudentModel
+                {
+                    Prenom = "Nicolas",
+                    Nom = "Vareille",
+                    Langage = "C#"
+                },
+                new StudentModel
+                {
+                    Prenom = "Timothée",
+                    Nom = "Nguyen",
+                    Langage = "C#"
+                },
+                new StudentModel
+                {
+                    Prenom = "Dylan",
+                    Nom = "Couto De Oliveira",
+                    Langage = "PHP"
+                },
+            });
+        }
+    }
+}
